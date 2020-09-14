76c7875c7b93e8cc51512dd56b473b322742c7ee
Rakotomalala01 <johary.rakotomalala@polymtl.ca>
2020-09-14 23:24:38 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -22,7 +22,9 @@ def average(a: float, b: float, c: float) -> float:
 
 
 def to_radians(angle_degs: float, angle_mins: float, angle_secs: float) -> float:
+    
     return 
+
 
 
 def to_degrees(angle_rads: float) -> tuple:
